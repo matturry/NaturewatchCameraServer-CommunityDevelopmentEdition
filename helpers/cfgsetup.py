161e54599c2c02bdcffa8cce897eeb57daa4e9ff
andysheen <andymsheen@gmail.com>
2024-12-03 12:43:51 -0500

    Update cfgsetup.py to use nmcli

--- helpers/cfgsetup.py
+++ helpers/cfgsetup.py
@@ -5,13 +5,13 @@ import os
 import subprocess
 
 # Get SSID and passphrase from hostapd.conf
-HOST_CONFIG_FILE_PATH = "/etc/hostapd/hostapd.conf"
+#HOST_CONFIG_FILE_PATH = "/etc/hostapd/hostapd.conf"
 
-with open(HOST_CONFIG_FILE_PATH, "r", encoding="utf-8") as config_file:
-    host_config_file = config_file.readlines()
+#with open(HOST_CONFIG_FILE_PATH, "r", encoding="utf-8") as config_file:
+#    host_config_file = config_file.readlines()
 
-current_ssid = host_config_file[2][5:].strip()
-print(f"hostapd configuration - SSID: {current_ssid}")
+#current_ssid = host_config_file[2][5:].strip()
+#print(f"hostapd configuration - SSID: {current_ssid}")
 
 # Generate a unique SSID based on the Pi's serial number
 unique_id = subprocess.check_output(
@@ -19,12 +19,8 @@ unique_id = subprocess.check_output(
 )
 unique_ssid = f"MyNaturewatch-{unique_id.strip().decode('utf-8')[-8:]}"
 
-if unique_ssid == current_ssid:
-    print("Unique SSID already set, no further action is needed.")
-else:
-    host_config_file[2] = f"ssid={unique_ssid}\n"
-    print("Updating hostapd config with unique SSID...")
-    with open(HOST_CONFIG_FILE_PATH, "w", encoding="utf-8") as config_file:
-        config_file.writelines(host_config_file)
-    print("Updated hostapd.conf.")
-    os.system("sudo reboot now")
+#if unique_ssid == current_ssid:
+#    print("Unique SSID already set, no further action is needed.")
+#else:
+print("Updating hotspot")
+os.system("sudo nmcli device wifi hotspot ssid " + unique_ssid + " password badgersandfoxes")
